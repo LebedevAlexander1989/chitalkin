73308cf3309716e171b16f04c60ba15a72014130
79855 <leb.alex123@gmail.com>
2023-12-12 00:41:22 +0300

    добавил описание метода add

--- adapters-library/api-library/src/main/java/org/example/api/controller/ApiLibraryController.java
+++ adapters-library/api-library/src/main/java/org/example/api/controller/ApiLibraryController.java
@@ -1,12 +1,16 @@
 package org.example.api.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 import org.example.api.controller.base.ApiLibrary;
 import org.example.api.dto.RequestBookDto;
 import org.example.api.dto.ResponseBookDto;
 import org.example.api.dto.ResponseStatusBookDto;
 import org.example.api.service.ApiLibraryService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,6 +33,22 @@ public class ApiLibraryController implements ApiLibrary {
     @Override
     @Operation(summary = "Добавить книгу")
     @PostMapping(value = "/add", produces = "application/json")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Добавление книги на полку",
+            required = true,
+            content = @Content(
+                    schema = @Schema(implementation = RequestBookDto.class),
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    examples = {
+                            @ExampleObject(
+                                    value = """
+                                            {
+                                              "name": "Игрок",
+                                              "numberShelf": 1,
+                                              "status": "FREE"
+                                            }
+                                            """,
+                                    summary = "Пример запроса на добавление книги"),
+                    }))
     public @ResponseBody ResponseBookDto add(@RequestBody RequestBookDto requestBookDto) {
         return apiLibraryService.add(requestBookDto);
 
--- adapters-library/api-library/src/main/java/org/example/api/dto/RequestBookDto.java
+++ adapters-library/api-library/src/main/java/org/example/api/dto/RequestBookDto.java
@@ -1,20 +1,14 @@
 package org.example.api.dto;
 
-import io.swagger.v3.oas.annotations.Parameter;
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.example.core.domain.StatusBook;
 
 @Getter
 @Setter
 public class RequestBookDto {
-
-    @Parameter(name = "id", description = "Уникальный идентификатор")
     private Integer id;
-    @Parameter(name = "Название книги")
     private String name;
-    @Parameter(name = "Номер полки")
     private Integer numberShelf;
-    @Parameter(name = "Статус книги")
     private StatusBook status;
-
 }
